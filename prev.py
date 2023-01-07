5a70aff9e452003bc8ea30a13dfb35b691b9e220
Riya Garg <96983874+riyaa14@users.noreply.github.com>
2023-01-08 04:06:43 +0530

    Add files via upload

---
+++ prev.py
@@ -0,0 +1,12 @@
+prev1 = {
+    "google": "",
+    "microsoft": "",
+    "meta": "",
+    "amazon": "",
+    "NTA": "",
+    "jac": "",
+    "josaa": "",
+    "devfolio": "",
+    "unstopHiring": "",
+    "unstopHack": ""
+}
